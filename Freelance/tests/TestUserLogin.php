b0b429557de9ed80257a5c590e79720a0a74cf3a
Juan Pablo Flores Galindo <juanfg@outlook.com>
2017-02-16 15:16:29 -0600

    Se anadio un test case

---
+++ Freelance/tests/TestUserLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
+
+class TestUserLogin extends TestCase
+{
+    public function testInsertAUserAndSignIn()
+    {
+        $this->visit('/register')
+            ->type('Juan', 'name')
+            ->type('juanfg@outlook.com', 'email')
+            ->type('secret', 'password')
+            ->type('secret', 'password_confirmation')
+            ->press('Register')
+            ->click('Logout')
+            ->visit('login')
+            ->type('juanfg@outlook.com', 'email')
+            ->type('secret', 'password')
+            ->press('Login')
+            ->seePageIs('/home');      
+    }
+}
